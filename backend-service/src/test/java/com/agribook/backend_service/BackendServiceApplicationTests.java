fea6575bd62cf403fd8b2e36976b8e3a1fe38844
Pramod Kolte <pramodkoltepatil@gmail.com>
2024-12-19 23:05:42 +0530

    Initial commit

---
+++ backend-service/src/test/java/com/agribook/backend_service/BackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agribook.backend_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
